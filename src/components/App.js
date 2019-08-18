3e9667b6e5e59ce347a3eabd25ce3bb14d5cc7bd
eadehemingway <eade.hemingway@gmail.com>
2019-08-18 19:22:08 +0300

    add comments to help students

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,74 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-export const App = () => (
-  <section>
-    <div id="chart" />
-    <div className="button-container">
-      <button id="increase" className="increase-btn">
-        +
-      </button>
-      <button id="decrease" className="decrease-btn">
-        -
-      </button>
-    </div>
-  </section>
-);
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { id: 1, radius: 20, x: 150, color: '#2F4F4F' },
+        { id: 2, radius: 10, x: 400, color: 'lightpink' },
+        { id: 3, radius: 40, x: 650, color: '#708090' }
+      ],
+      staticCircleRadius: 100
+    };
+  }
+  componentDidMount() {
+    // in here we need to create a group for each of the three bits of data,
+    // bind these groups to the data
+    // and create the three circles for each group
+  }
+
+  componentDidUpdate() {
+    this.redrawLiquid()
+  }
+
+  redrawLiquid = () => {
+    // in here you should rebind the groups with the updated data
+    // and then re trigger the calculations of the properties that need to be updated
+  };
+
+  updateData = direction => {
+    const { data, staticCircleRadius } = this.state;
+
+    const newData = data.map(d => {
+      const newValue = direction === 'increase' ? d.radius + 10 : d.radius - 10;
+
+      const checkedValue =
+        newValue < 0
+          ? 0
+          : newValue > staticCircleRadius
+          ? staticCircleRadius
+          : newValue;
+
+      return { ...d, radius: checkedValue };
+    });
+    this.setState({ data: newData });
+  };
+
+  render() {
+    return (
+      <section>
+        <div id="chart">
+          <svg id="svg" />
+        </div>
+        <div className="button-container">
+          <button
+            id="increase"
+            className="increase-btn"
+            onClick={() => this.updateData('increase')}
+          >
+            +
+          </button>
+          <button
+            id="decrease"
+            className="decrease-btn"
+            onClick={() => this.updateData('decrease')}
+          >
+            -
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
